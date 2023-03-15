bd601f11d0544c94aca5ab7af941b345303cccb4
sj921 <111365047+sj921@users.noreply.github.com>
2023-03-16 03:17:00 +0900

    [level 0] Title: 두 수의 곱, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;    
+    if (0 <= num1 <= 100 && 0 <= num2 <= 100) {
+        answer = num1 * num2;
+        return answer; 
+    }
+}
