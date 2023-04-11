cecabbeabeef6a477fba4e6ac64d76f204549ff9
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 02:35:23 +0900

    Delete 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기 directory

--- 프로그래머스/unrated/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++
@@ -1,8 +0,0 @@
--- 코드를 입력하세요
-SELECT flavor
-FROM first_half f
-INNER JOIN icecream_info i
-USING (flavor)
-WHERE f.total_order > 3000
-AND i.ingredient_type = 'fruit_based'
-ORDER BY f.total_order DESC;
