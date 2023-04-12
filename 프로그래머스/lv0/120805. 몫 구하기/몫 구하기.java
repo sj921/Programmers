269002f95cd3657188660cf5f7c1a2f1a80b4621
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:28:45 +0900

    Update 몫 구하기.java

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -1,7 +1,9 @@
-public int Solution(int num1, int num2) {
-    int answer = 0;    
-    if(0 < num1 < 100 && 0 < num2 <= 100){
-        int answer = num1 / num2;                
-        return answer;
-    } 
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;    
+        if(0 < num1 < 100 && 0 < num2 <= 100){
+            int answer = num1 / num2;                
+            return answer;
+        } 
+    }
 }
