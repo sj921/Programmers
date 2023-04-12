eff1409f5538cbc229a5aecb14f9ca6d585b13fa
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:25:00 +0900

    Update and rename 나머지 구하기.c to 나머지 구하기.java

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
+public int Solution(int num1, int num2) {
     int answer = 0;
     if (0 <= num1 <= 100 && 0 <= num2 <= 100){
         return answer= num1 % num2;
