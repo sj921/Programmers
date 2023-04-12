ac12483a3bd370394d2a3f14dcf17e65fe461a71
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:27:46 +0900

    Update 두 수의 차.java

--- 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -1,7 +1,9 @@
-public int Solution(int num1, int num2) {
-    int answer = 0;
-    if (-50000 <= num1 <= 50000 && -50000 <= num2 <= 50000){
-        answer = num1 - num2;            
-        return answer;        
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (-50000 <= num1 <= 50000 && -50000 <= num2 <= 50000){
+            answer = num1 - num2;            
+            return answer;        
+        }
     }
 }
