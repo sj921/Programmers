17b7e9677325c1a6a0dd28e2511f48583b699e5b
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:30:05 +0900

    Update 나머지 구하기.java

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,6 +1,8 @@
-public int Solution(int num1, int num2) {
-    int answer = 0;
-    if (0 <= num1 <= 100 && 0 <= num2 <= 100){
-        return answer= num1 % num2;
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (0 <= num1 <= 100 && 0 <= num2 <= 100){
+            return answer= num1 % num2;
+        }
     }
 }
