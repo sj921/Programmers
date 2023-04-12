c5c943b3cfe10c60325f77acfac8661c715ae6aa
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:29:45 +0900

    Update 숫자 비교하기.java

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,4 +1,6 @@
-public int Solution(int num1, int num2) {
-  int answer = (num1 == num2) ? 1 : -1;
-  return answer;
+class Solution {
+  public int solution(int num1, int num2) {
+    int answer = (num1 == num2) ? 1 : -1;
+    return answer;
+  }
 }
