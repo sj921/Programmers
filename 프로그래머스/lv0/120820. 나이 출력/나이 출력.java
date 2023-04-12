cb3e5361339769843765921fb334f1c873116a8b
Susie <111365047+sj921@users.noreply.github.com>
2023-04-12 20:26:15 +0900

    Update and rename 나이 출력.c to 나이 출력.java

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        if (0 < age <= 120){
+            return answer = year - age + 1;
+        } 
+    } 
+}
