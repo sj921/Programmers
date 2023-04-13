e31a6def863effe644b6c4dfce858342d66fb2b3
Susie <111365047+sj921@users.noreply.github.com>
2023-04-14 02:32:00 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.27 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(int)Math.ceil((double)n/2)];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                answer[i / 2] = i;
+            }
+        }
+        return answer;
+    }
+}
