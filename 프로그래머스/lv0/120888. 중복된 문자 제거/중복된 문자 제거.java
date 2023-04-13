001b972a2af2d57fc169a4adccd0dc890423a6f8
Susie <111365047+sj921@users.noreply.github.com>
2023-04-14 03:20:34 +0900

    [level 0] Title: 중복된 문자 제거, Time: 11.41 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.indexOf(my_string.charAt(i)) == i) {
+                answer += my_string.charAt(i);
+                // System.out.println(my_string.charAt(i));
+                // System.out.println(my_string.indexOf(my_string.charAt(i)));
+            }
+        }
+        return answer;
+    }
+}
