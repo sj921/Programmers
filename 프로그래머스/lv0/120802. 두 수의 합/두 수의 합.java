62b8c16e6542acce8121740447247626fb0a2cff
Susie <111365047+sj921@users.noreply.github.com>
2023-04-25 01:57:39 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.java
@@ -1,9 +1,5 @@
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = -1;    
-        if (-50000 <= num1 <= 50000 && -50000 <= num2 <= 50000){
-            answer = num1 + num2;        
-            return answer;
-        }
+    public int solution(int num1, int num2) {  
+        return num1 + num2;    
     }
 }
