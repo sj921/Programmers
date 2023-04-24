6b20d3e0771aaea5e4a410605e12abcc070a1c23
Susie <111365047+sj921@users.noreply.github.com>
2023-04-25 02:07:18 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.java
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;    
-        if (0 <= num1 <= 100 && 0 <= num2 <= 100) {
-            answer = num1 * num2;
-            return answer; 
-        }
+        return num1 * num2;
     }
 }
