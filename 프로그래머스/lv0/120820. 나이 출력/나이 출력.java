79339d32bc596d9d2bb5879d3981b726aa2b799d
Susie <111365047+sj921@users.noreply.github.com>
2023-05-12 02:41:33 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int age) {
-        int answer = 0;
         int year = 2022;
-        if (0 < age <= 120){
-            return answer = year - age + 1;
-        } 
-    } 
+        return year - age + 1;
+    }
 }
