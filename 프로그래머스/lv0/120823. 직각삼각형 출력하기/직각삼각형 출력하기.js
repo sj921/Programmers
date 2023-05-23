ec982e387b163a06ee8c54aaad93dd45885f6732
Susie <111365047+sj921@users.noreply.github.com>
2023-05-24 02:59:15 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 47.38 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -1,4 +1,7 @@
+// readline 모듈 불러오기
 const readline = require('readline');
+
+// 인터페이스 객체 만들기
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -6,14 +9,16 @@ const rl = readline.createInterface({
 
 let input = [];
 
+// 입출력 코드 작성하기
 rl.on('line', function (line) {
+	// 입력 받은 값을 처리하는 코드
     input = line.split(' ');
 }).on('close', function () {
-    solution(Number(input[0]));
-});
-
-function solution(n) {
-    for(let i = 1; i < n+1; i++) {
+	// 입력이 끝나고 실행하는 코드
+    // console.log(Number(input[0]));
+    for (let i = 1; i <= input[0]; i++) {
         console.log('*'.repeat(i));
     }
-}
+});
+
+// 실행하고 종료할 때마다 i의 값이 증가하고 각 줄마다 *이 추가로 생성된다
