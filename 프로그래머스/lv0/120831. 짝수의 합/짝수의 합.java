4edc61f8039a01afdf705c54c1235783bd7f270b
Susie <111365047+sj921@users.noreply.github.com>
2023-05-25 02:18:39 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;       
+        int answer = 0;  
+        
         for (int i = 2; i <= n; i += 2) {
             answer += i;
         }      
+        
         return answer;
     }
 }
