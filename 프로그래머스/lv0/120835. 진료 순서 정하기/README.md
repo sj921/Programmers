d78adb0db6273d60a404367ed8d70a195afbd588
Susie <111365047+sj921@users.noreply.github.com>
2023-05-25 03:35:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/README.md
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 진료 순서 정하기 - 120835 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120835) 
 
 ### 성능 요약
 
-메모리: 70.9 MB, 시간: 0.02 ms
+메모리: 78.6 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
-        
+        int[] answer = new int[emergency.length];       
         for (int i = 0; i < emergency.length; i++) {
+            answer[i]++;    // answer[i]를 1로 초기화
             for (int j = 0; j < emergency.length; j++) {
-                if (emergency[i] < emergency[j]) {
+                if (emergency[i] < emergency[j]) { // 응급도가 더 낮은 경우 응급도의 크기를 증가
                     answer[i]++;
-                }            
+                } 
             }
-            answer[i]++;
-        }
-                
+        }                    
         return answer;
     }
 }
