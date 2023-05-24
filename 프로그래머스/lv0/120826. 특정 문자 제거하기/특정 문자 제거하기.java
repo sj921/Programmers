f6a73ab5d771c6a0a13e9f191337e0c25d0718a2
Susie <111365047+sj921@users.noreply.github.com>
2023-05-25 01:34:12 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.28 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,5 +1,14 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        return my_string.replaceAll(letter, "");
+        String[] answer = new String[my_string.length()];
+        answer = my_string.split("");
+        
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i].equals(letter)) {
+                answer[i] = "";
+            }
+        }
+        
+        return String.join("", answer);
     }
 }
