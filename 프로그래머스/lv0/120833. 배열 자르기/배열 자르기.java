4f49cbce3b58b073069b227c41050f77c0030d87
Susie <111365047+sj921@users.noreply.github.com>
2023-05-25 03:06:43 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,11 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-        
-        for (int i = num1, j = 0; i <= num2; i++, j++) {
-            answer[j] = numbers[i];       
-        }   
-        
+        int[] answer = new int[numbers.length];
+        answer = Arrays.copyOfRange(numbers, num1, num2 + 1);        
         return answer;
     }
 }
+
+
+
+// class Solution {
+//     public int[] solution(int[] numbers, int num1, int num2) {
+//         int[] answer = new int[num2 - num1 + 1];
+        
+//         for (int i = num1, j = 0; i <= num2; i++, j++) {
+//             answer[j] = numbers[i]; 
+//             // System.out.println("j : " + j);
+//             // System.out.println("numbers[i] : " + numbers[i]);
+//         }   
+        
+//         return answer;
+//     }
+// }
