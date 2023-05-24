296bca38838c89b2be37bc36402360529ee97387
Susie <111365047+sj921@users.noreply.github.com>
2023-05-25 03:06:04 +0900

    [level 0] Title: 배열 자르기, Time: 2.95 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,3 +1,11 @@
-function solution(numbers, num1, num2) { 
-    return numbers.slice(num1, num2+1);
+function solution(numbers, num1, num2) {
+    let answer = [];
+    
+    for (let i = num1; i <= num2; i++) {
+        answer.push(numbers[i]);        
+        console.log("i : " + i);
+        console.log("numbers[i] : " + numbers[i]);
+    }    
+    
+    return answer;
 }
