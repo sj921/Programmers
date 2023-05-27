2b4f50274e251d65e78006e32d8f07c3d0d1fb71
Susie <111365047+sj921@users.noreply.github.com>
2023-05-27 18:54:26 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,18 @@
+SELECT 
+    ANIMAL_ID,
+    NAME
+FROM ( SELECT
+            INS.ANIMAL_ID,
+            INS.NAME
+        FROM
+            ANIMAL_INS INS
+        INNER JOIN
+            ANIMAL_OUTS OUTS
+        ON
+            INS.ANIMAL_ID = OUTS.ANIMAL_ID
+        ORDER BY
+            OUTS.DATETIME - INS.DATETIME DESC 
+     )
+WHERE ROWNUM <= 2;
+
+    
