8c91c2603bef270b7e70aa2a0b549f9f32c0b0f6
Susie <111365047+sj921@users.noreply.github.com>
2023-06-02 01:41:59 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int hp) {
-        int answer = (hp/5) + (hp%5)/3 + ((hp%5)%3)/1;
-     
+        int answer = (hp/5) + (hp%5)/3 + ((hp%5)%3)/1;     
         return answer;
     }
 }
